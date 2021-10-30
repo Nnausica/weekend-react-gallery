802607f6a8aa37fce861eda7e81cfb826f999106
Laura Burbank <laura.burbank@gmail.com>
2021-10-30 17:45:20 -0500

    likes change and count up

--- src/components/App/GalleryItems/GalleryItems.jsx
+++ src/components/App/GalleryItems/GalleryItems.jsx
@@ -6,11 +6,25 @@ function GalleryItems(props){
     const [showImage, setShowImage]= useState(true);
     // variable= show image, function to run= setShowimage, useState= initial value
 
+    const [likes, setlikes]= useState( 0 );
+    //creates a counter
+
+    const [anylikes, setanylikes]= useState(false);
+    //set the status of anylikes
+
     const toggleImage = ()=>{
         console.log( 'in toggle image');
         setShowImage(!showImage);
     }
     //function to toggle images
+
+    const addlikes= ()=>{
+        console.log('in setlikes', likes+1)
+        setlikes( likes + 1)
+        if(likes>=0){setanylikes(true)}
+    }
+    //function to switch between likes options on DOM
+
 
     return(
         <div>
@@ -22,6 +36,17 @@ function GalleryItems(props){
                 <img src={props.image.path} alt={props.image.description} onClick={ toggleImage }/>://true
                 <h2 onClick={ toggleImage}>{props.image.description}</h2> //false
             }
+
+        
+        <div>
+            <button onClick={addlikes}>Like</button> 
+            {//shows the number of likes for each image
+                anylikes ?
+                <p> {likes} likes </p>: //true
+                <p> No one likes this image yet! </p> //false     
+            }
+        </div>
+            
 
 
         </div>
